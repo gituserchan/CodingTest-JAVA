141e249ba552100b23efd1cc2c02c6526a856e8a
Parkchanjin <137139516+gituserchan@users.noreply.github.com>
2025-10-05 15:04:51 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(" ".repeat(N - i));
+            sb.append("*".repeat(2 * i - 1));
+            sb.append("\n");
+        }
+
+        for (int i = N - 1; i >= 1; i--) {
+            sb.append(" ".repeat(N - i));
+            sb.append("*".repeat(2 * i - 1));
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
