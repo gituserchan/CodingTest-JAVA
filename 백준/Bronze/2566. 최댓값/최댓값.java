270a3f989701035b452214d80c172858a9bf5278
Parkchanjin <137139516+gituserchan@users.noreply.github.com>
2025-10-06 16:30:12 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 16004 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] arr = new int[9][9];
+        int max = Integer.MIN_VALUE;
+        int x = 0, y = 0;
+
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()); // () 필수!
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.println((x + 1) + " " + (y + 1));
+    }
+}
